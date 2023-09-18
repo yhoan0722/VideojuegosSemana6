79decfaabd7654785da6b91c921afcc32a0ac2a7
yhoan0722 <yhonatan12456@gmail.com>
2023-09-17 23:29:57 -0500

    add

--- Assets/Scripts/MenuJuego.cs
+++ Assets/Scripts/MenuJuego.cs
@@ -5,40 +5,60 @@ using UnityEngine.SceneManagement;
 
 public class MenuJuego : MonoBehaviour
 {
+    // Variables para referenciar objetos de la interfaz de usuario
     [SerializeField] private GameObject botonPausa;
     [SerializeField] private GameObject menuPausa;
+
+    // Variable para rastrear si el juego está pausado o no
     private bool juegoPausado = false;
 
-    private void Update(){
-        if(Input.GetKeyUp(KeyCode.Space)){
-            if(juegoPausado){
+    private void Update()
+    {
+        // Verifica si la tecla Espacio se ha soltado
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            // Si el juego está pausado, lo reanuda; de lo contrario, lo pausa
+            if (juegoPausado)
+            {
                 Reanudar();
             }
-            else{
+            else
+            {
                 Pausa();
             }
         }
     }
 
-    public void Pausa(){
-        juegoPausado = true;
-        Time.timeScale = 0f;
-        botonPausa.SetActive(false);
-        menuPausa.SetActive(true);
+    // Método para pausar el juego
+    public void Pausa()
+    {
+        juegoPausado = true;  // Marca el juego como pausado
+        Time.timeScale = 0f; // Detiene el tiempo en el juego
+        botonPausa.SetActive(false); // Oculta el botón de pausa
+        menuPausa.SetActive(true);   // Muestra el menú de pausa
     }
 
-    public void Reanudar(){
-        juegoPausado=false;
-        Time.timeScale = 1f;
-        botonPausa.SetActive(true);
-        menuPausa.SetActive(false);
+    // Método para reanudar el juego
+    public void Reanudar()
+    {
+        juegoPausado = false;  // Marca el juego como no pausado
+        Time.timeScale = 1f;   // Reanuda el tiempo en el juego
+        botonPausa.SetActive(true);  // Muestra el botón de pausa
+        menuPausa.SetActive(false);  // Oculta el menú de pausa
     }
 
-    public void Reiniciar(){
-        Time.timeScale = 1f;
+    // Método para reiniciar el nivel
+    public void Reiniciar()
+    {
+        Time.timeScale = 1f;  // Reanuda el tiempo en el juego
+        // Carga la escena actual (reinicia el nivel)
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
-    public void Salir(){
+
+    // Método para salir del nivel y volver a la escena anterior
+    public void Salir()
+    {
+        // Carga la escena anterior en la secuencia de construcción de escenas
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 }
--- Assets/Scripts/MenuPrincipal.cs
+++ Assets/Scripts/MenuPrincipal.cs
@@ -5,13 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class MenuPrincipal : MonoBehaviour
 {
-   public void Jugar(){
-      Time.timeScale = 1f;
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-   }
+   // Este método es llamado cuando el botón "Jugar" es presionado
+    public void Jugar()
+    {
+        // Restablece el tiempo del juego a su velocidad normal (1x)
+        Time.timeScale = 1f;
 
-   public void Salir(){
-    Debug.Log("Salir.........");
-    Application.Quit();
-   }
+        // Carga la siguiente escena en el orden del Build Settings
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    // Este método es llamado cuando el botón "Salir" es presionado
+    public void Salir()
+    {
+        Debug.Log("Salir.........");
+
+        // Cierra la aplicación
+        Application.Quit();
+    }
 }
